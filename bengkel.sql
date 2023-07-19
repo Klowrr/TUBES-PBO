e2a7354144ff75f4b882ac6759afb1f19ac35fa2
Stefanus Gratilio <121921500+stefanus-gratilio-05@users.noreply.github.com>
2023-07-19 16:07:12 +0700

    Update bengkel.sql
    Changing line 21 from:
    -- Database: `bengkel2`
    
    into:
    -- Database: `bengkel`

--- bengkel.sql
+++ bengkel.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bengkel2`
+-- Database: `bengkel`
 --
 CREATE DATABASE IF NOT EXISTS `bengkel` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `bengkel`;
