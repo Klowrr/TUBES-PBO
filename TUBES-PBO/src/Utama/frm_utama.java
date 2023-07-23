a3782f295e3c5a69e387b08224ea8fb80a707f1f
Apriana <aprianacahya04@gmail.com>
2023-07-23 17:43:13 +0700

    build

--- TUBES-PBO/src/Utama/frm_utama.java
+++ TUBES-PBO/src/Utama/frm_utama.java
@@ -4,6 +4,8 @@ package Utama;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+import java.io.FileInputStream;
+
 import Panel.panel_users;
 import Panel.panel_transaksi;
 import Panel.panel_service;
@@ -293,21 +295,21 @@ public class frm_utama extends javax.swing.JFrame {
     // End of variables declaration//GEN-END:variables
 
     private void execute() {
-        ImageIcon iconMaster = new ImageIcon(getClass().getResource("../img/icon_Stack.png"));
-        ImageIcon iconBarang = new ImageIcon(getClass().getResource("../img/icon_product.png"));
-        ImageIcon iconClient = new ImageIcon(getClass().getResource("../img/icon_client.png"));
-        ImageIcon iconMekanik = new ImageIcon(getClass().getResource("../img/icon_tools_mekanik.png"));
-        ImageIcon iconService = new ImageIcon(getClass().getResource("../img/icon_service.png"));
+        ImageIcon iconMaster = new ImageIcon(getClass().getResource("/img/icon_Stack.png"));
+        ImageIcon iconBarang = new ImageIcon(getClass().getResource("/img/icon_product.png"));
+        ImageIcon iconClient = new ImageIcon(getClass().getResource("/img/icon_client.png"));
+        ImageIcon iconMekanik = new ImageIcon(getClass().getResource("/img/icon_tools_mekanik.png"));
+        ImageIcon iconService = new ImageIcon(getClass().getResource("/img/icon_service.png"));
 
-        ImageIcon iconTransaksi = new ImageIcon(getClass().getResource("../img/icon_transaction.png"));
-        ImageIcon iconBarangMasuk = new ImageIcon(getClass().getResource("../img/icon_barang_masuk.png"));
+        ImageIcon iconTransaksi = new ImageIcon(getClass().getResource("/img/icon_transaction.png"));
+        ImageIcon iconBarangMasuk = new ImageIcon(getClass().getResource("/img/icon_barang_masuk.png"));
        
-        ImageIcon iconHistory = new ImageIcon(getClass().getResource("../img/icon_history.png"));
+        ImageIcon iconHistory = new ImageIcon(getClass().getResource("/img/icon_history.png"));
         
-        ImageIcon iconIncome = new ImageIcon(getClass().getResource("../img/icon_income.png"));
-        ImageIcon iconOutcome = new ImageIcon(getClass().getResource("../img/icon_outcome.png"));
+        ImageIcon iconIncome = new ImageIcon(getClass().getResource("/img/icon_income.png"));
+        ImageIcon iconOutcome = new ImageIcon(getClass().getResource("/img/icon_outcome.png"));
 
-        ImageIcon iconUsers = new ImageIcon(getClass().getResource("../img/icon_user3.png"));
+        ImageIcon iconUsers = new ImageIcon(getClass().getResource("/img/icon_user3.png"));
             
         menu_item barang = new menu_item(null,true,iconBarang,"Barang",new ActionListener() {
             @Override
